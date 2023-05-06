179a097e9314d5661a6ce90416d41e1343237a22
João Vitor Pastori <124517985+JvPastori@users.noreply.github.com>
2023-05-05 22:22:09 -0300

    Update and rename sla3.java to intervalo.java

--- intervalo.java
+++ intervalo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class sla3{
+public class intervalo{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         double a= sc.nextDouble();
