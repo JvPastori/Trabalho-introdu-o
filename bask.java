103f7253ff49d088cf6b57813e5921a4d27c761d
João Vitor Pastori <124517985+JvPastori@users.noreply.github.com>
2023-05-05 22:21:40 -0300

    Update and rename sla2.java to bask.java

--- bask.java
+++ bask.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
-public class sla2{
+public class bask{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
